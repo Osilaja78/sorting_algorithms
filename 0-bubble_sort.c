6e86674c0d2840039ef1b99307085253e1d09e6c
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-23 15:25:57 +0100

    fixed 0-O file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,8 +18,8 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = false;
-		for (j = 0; j < size - 1 - i; j++)
-	{
+		for (j = 0; j < size - 1; j++)
+		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
