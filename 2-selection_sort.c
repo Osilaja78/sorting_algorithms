5112ad58cabb09ffee7f58b036bb6ddcd5b5824f
Hameed Osilaja <osilajaabdulhameed@gmail.com>
2023-07-24 06:29:19 +0100

    code for task 2 modified

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * selection_sort - sorts an array of integers in ascending order.
@@ -10,6 +11,7 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min, temp;
+	bool swap;
 
 	if (array == NULL)
 		return;
@@ -17,14 +19,21 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		min = i;
+		swap = false;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
+			{
 				min = j;
+				swap = true;
+			}
 		}
-		temp = array[min];
-		array[min] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		if (swap == true)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
